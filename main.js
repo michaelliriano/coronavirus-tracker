e6806b5025f29494e980523b69770b497ab6da6c
Michael Liriano <51761089+michaelliriano@users.noreply.github.com>
2020-04-16 20:42:20 -0400

    made it better

--- main.js
+++ main.js
@@ -2,37 +2,48 @@ function logSubmit(e) {
   var input = document.getElementById('getCountry').value;
   e.preventDefault();
   getCountryCase(input);
-  console.log(input);
 }
 
 function getCountryCase(input) {
   var settings = {
-    url: `https://api.covid19api.com/live/country/${input}/status/confirmed`,
+    url: `https://api.covid19api.com/summary`,
     method: 'GET',
     timeout: 0,
   };
 
   $.ajax(settings).done(function (response) {
-    let countries = response.length - 1;
-    let confirmed = response[countries].Confirmed;
-    let deaths = response[countries].Deaths;
-    let recovered = response[countries].Recovered;
-    let active = response[countries].Active;
-    let country = response[countries].Country;
-    let conh3 = document.getElementById('log');
-    conh3.innerHTML = `
+    let ourCountry = response.Countries.filter(function (usCountry) {
+      return usCountry.Country == input;
+    });
+
+    let origin = ourCountry[0].Country;
+    let newConfirmed = ourCountry[0].NewConfirmed;
+    let newDeaths = ourCountry[0].NewDeaths;
+    let newRecovered = ourCountry[0].NewRecovered;
+    let totalConfirmed = ourCountry[0].TotalConfirmed;
+    let totalDeaths = ourCountry[0].TotalDeaths;
+    let totalRecovered = ourCountry[0].TotalRecovered;
+
+    let countryFin = document.getElementById('log');
+    countryFin.innerHTML = `
       <br>
-      <h3>There are <span class="confirmed">${confirmed}</span> confirmed cases in ${country}  </h3> 
+      <h3><span class="confirmed">${totalConfirmed}</span> Confirmed Cases in ${origin}  </h3> 
       <br>
-      <h3>There are <span class="deaths">${deaths}</span> deaths in ${country} </h3> 
+      <h3><span class="deaths">${totalDeaths}</span> Deaths in ${origin} </h3> 
       <br>
-      <h3>There are <span class="recovered">${recovered}</span> recovered cases in ${country}  </h3> 
+      <h3><span class="recovered">${totalRecovered}</span> Recovered Cases in ${origin}  </h3> 
       <br>
-      <h3 class="active">Currently Active: ${active}</h3>
+      <h3 class="active">Currently Active: ${totalConfirmed}</h3>
       
       `;
   });
+  resetCountryValue();
 }
+function resetCountryValue() {
+  document.getElementById('getCountry').value = '';
+  document.getElementById('getCountry').focus();
+}
+
 const countryForm = document.getElementById('form');
 countryForm.addEventListener('submit', logSubmit);
 
@@ -41,8 +52,11 @@ countryForm.addEventListener('submit', logSubmit);
 function logSubmitTwo(e) {
   var input2 = document.getElementById('getState').value;
   e.preventDefault();
-  console.log(input2);
   getStateCase(input2);
+}
+function resetStateValue() {
+  document.getElementById('getState').value = '';
+  document.getElementById('getState').focus();
 }
 
 function getStateCase(input2) {
@@ -74,6 +88,7 @@ function getStateCase(input2) {
       
       `;
   });
+  resetStateValue();
 }
 
 const stateForm = document.getElementById('formTwo');
